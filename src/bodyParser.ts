27b1921df2c5aab1517bf43aa28525fdeebb9131
Mirko <mirkomassa18@gmail.com>
2023-01-14 00:45:17 +0100

    section size parser fixed

--- src/bodyParser.ts
+++ src/bodyParser.ts
@@ -6,11 +6,9 @@ export class parsedBody{
 }
 export function parseType(bytes: Uint8Array, index: number): parsedBody{
     //checking the number of function signatures
-    const [size, width] = lebToInt(bytes.slice(index+1, index+1+4)); //size is the number of functions signatures in the module
+    const [size, width] = lebToInt(bytes.slice(index, index+4)); //size is the number of functions signatures in the module
     index+= width; //offset after the number of function signatures declaration
-    console.log(bytes[index])
     if(bytes[index] != 0x60) throw new Error("No vector of function signatures.")
-
     const pb = new parsedBody(size, []);
     index++; //offset at the start of the first function signature
 
@@ -19,15 +17,17 @@ export function parseType(bytes: Uint8Array, index: number): parsedBody{
             // parameter (number and types)
             let [size, width] = lebToInt(bytes.slice(index, index+4));
             index+=width;
-            const params:types.tupleType = [size, bytes.slice(index+1, index+1+size)];
+            const params:types.tupleType = [size, bytes.slice(index, index+size)];
             index+=size;
             // return values (number and types)
             [size, width] = lebToInt(bytes.slice(index, index+4));
             index+=width;
-            const returns:types.tupleType = [size, bytes.slice(index+1, index+1+size)];
+            const returns:types.tupleType = [size, bytes.slice(index, index+size)];
             // function
             const func:types.funcType = {parameters:params, returns:returns}
             pb.elements.push(func)
         }
     return pb; 
 }
+
+// console.log(JSON.stringify(parseType(new Uint8Array([0x01, 0x60, 0x02, 0x7F, 0x7F, 0x01, 0x7F]), 0)))
