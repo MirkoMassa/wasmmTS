48c13dbf9ae6adbd37f6385898977094bfe38db8
Mirko <mirkomassa18@gmail.com>
2023-04-27 02:46:49 +0200

    ignoring built .js files on testing (I should've discovered this before

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 console.log("DIRECTORY", __dirname);
 console.log("CWD", process.cwd());
 module.exports = {
+  testPathIgnorePatterns: [".js"],
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
